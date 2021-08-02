4b8550d708f3e3877c5232171968e9d95a1603f3
Juliano Ferreira Alves <58094452+julianofalves31@users.noreply.github.com>
2021-08-02 00:36:36 -0300

    Update CBO1.bat

--- CBO1.bat
+++ CBO1.bat
@@ -4,7 +4,7 @@
 ping www.google.com -n 1 || goto Loop
 
 :Attempt
-C:\windows\system32\rasdial.exe bithelp urca urc@1234 && goto Connected || goto Failed
+C:\windows\system32\rasdial.exe {nome da conexão} {usuario} {senha} && goto Connected || goto Failed
 
 :Failed
 echo Connection failed on %date% %time% > c:\vpnlog.txt
@@ -14,6 +14,6 @@ goto Attempt
 echo Connection completed on %date% %time% > c:\vpnlog.txt
 
 :conectardp
-@echo Conectando ao Host : 10.10.10.1
-mstsc /v: 10.10.10.1
+@echo Conectando ao Host : {IP}
+mstsc /v: {IP}
 @echo conexao realizada com sucesso!
